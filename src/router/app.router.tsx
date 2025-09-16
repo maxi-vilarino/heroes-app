5d4e263222b849c03d588c127a6701480be27f85
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-09-16 10:04:03 +0200

    Creación de Layouts

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,24 +1,39 @@
+import { createBrowserRouter } from "react-router";
 import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { SearchPage } from "@/heroes/pages/search/SearchPage";
-import { createBrowserRouter } from "react-router";
+import { AdminLayout } from "../admin/layout/AdminLayout";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/heroes/1",
+        element: <HeroPage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+    ],
   },
-  {
-    path: "/heroes/1",
-    element: <HeroPage />,
-  },
-  {
-    path: "/search",
-    element: <SearchPage />,
-  },
+
   {
     path: "/admin",
-    element: <AdminPage />,
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />,
+      },
+    ],
   },
 ]);
