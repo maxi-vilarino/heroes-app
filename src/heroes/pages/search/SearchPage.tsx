e83c7675268b158ef0f5fbea55f7613a61d1e30d
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-09-17 09:10:13 +0200

    Creación de Heroes Home Page y componente Header

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,14 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <CustomJumbotron
+        title="Superhero Search"
+        description="Discover, explore, and manage your favorite superheroes and villains"
+      />
+    </>
+  );
 };
 
 export default SearchPage;
