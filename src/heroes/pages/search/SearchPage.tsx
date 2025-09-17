1f4a098912a3a5a7fe7d37da28ee1b6b051354ae
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-09-17 09:33:53 +0200

    Creación del componente Heroe Stat Card

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,12 +1,17 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
 
 export const SearchPage = () => {
   return (
     <>
+      {/* Header */}
       <CustomJumbotron
         title="Superhero Search"
         description="Discover, explore, and manage your favorite superheroes and villains"
       />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
     </>
   );
 };
