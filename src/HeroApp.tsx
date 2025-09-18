3e8e909ae843425afb25e6b5383fa0da75c4cafd
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-09-18 10:23:18 +0200

    Tipado de la respuesta y creación de la función para traer los heroes

--- src/HeroApp.tsx
+++ src/HeroApp.tsx
@@ -1,10 +1,15 @@
 import { RouterProvider } from "react-router";
 import { appRouter } from "./router/app.router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// Create a client
+const queryClient = new QueryClient();
 
 export const HeroApp = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={appRouter} />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -1,22 +1,25 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
 import { HeroeGrid } from "@/heroes/components/HeroeGrid";
 import { CustomPagination } from "@/components/custom/CustomPagination";
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
-import { getHeroesByPage } from "@/heroes/actions/get-heroes-by-page.actions";
+import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.actions";
 
 export const HomePage = () => {
   const [activeTab, setActiveTab] = useState<
     "all" | "favorites" | "heroes" | "villains"
   >("all");
 
-  useEffect(() => {
-    getHeroesByPage().then((heroes) => {
-      console.log({ heroes });
-    });
-  }, []);
+  const { data } = useQuery({
+    queryKey: ["heroes"],
+    queryFn: () => getHeroesByPageAction(),
+    staleTime: 1000 * 60 * 5, // 5 minutos
+  });
+
+  console.log({ data });
 
   return (
     <>
