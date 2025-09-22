30096dbdb8ba247c3aa44712398a79cd4e2e3515
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-09-22 09:06:23 +0200

    Implementación de page y limite

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -12,6 +12,8 @@ export const HomePage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const activeTab = searchParams.get("tab") ?? "all";
+  const page = searchParams.get("page") ?? "1";
+  const limit = searchParams.get("limit") ?? "6";
 
   const selectedTab = useMemo(() => {
     const validTab = ["all", "favorites", "heroes", "villains"];
@@ -20,7 +22,7 @@ export const HomePage = () => {
 
   const { data: heroesResponse } = useQuery({
     queryKey: ["heroes"],
-    queryFn: () => getHeroesByPageAction(),
+    queryFn: () => getHeroesByPageAction(+page, limit),
     staleTime: 1000 * 60 * 5, // 5 minutos
   });
 
