c1a37ae1df798c5f6717dc61ce0f0ef3b25c6d6f
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-09-24 09:21:02 +0200

    Creación del contexto de favoritos

--- src/HeroApp.tsx
+++ src/HeroApp.tsx
@@ -2,14 +2,17 @@ import { RouterProvider } from "react-router";
 import { appRouter } from "./router/app.router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { FavoriteHeroProvider } from "./heroes/context/FavoriteHeroContext";
 // Create a client
 const queryClient = new QueryClient();
 
 export const HeroApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={appRouter} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <FavoriteHeroProvider>
+        <RouterProvider router={appRouter} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </FavoriteHeroProvider>
     </QueryClientProvider>
   );
 };
---
+++ src/heroes/context/FavoriteHeroContext.tsx
@@ -0,0 +1,59 @@
+import {
+  createContext,
+  useEffect,
+  useState,
+  type PropsWithChildren,
+} from "react";
+import type { Hero } from "../types/hero.interface";
+
+interface FavoriteHeroContext {
+  favorites: Hero[];
+  favoriteCount: number;
+
+  toggleFavorite: (hero: Hero) => void;
+  isFavorite: (hero: Hero) => boolean;
+}
+
+export const FavoriteHeroContext = createContext({} as FavoriteHeroContext);
+
+const getFavoritesFromLocalStorage = (): Hero[] => {
+  const favorites = localStorage.getItem("favorites");
+  return favorites ? JSON.parse("favorites") : [];
+};
+
+export const FavoriteHeroProvider = ({ children }: PropsWithChildren) => {
+  const [favorites, setFavorites] = useState<Hero[]>(
+    getFavoritesFromLocalStorage()
+  );
+
+  const toggleFavorites = (hero: Hero) => {
+    const heroExist = favorites.find((h) => h.id === hero.id);
+    if (heroExist) {
+      const newFavorites = favorites.filter((h) => h.id != hero.id);
+      setFavorites(newFavorites);
+      return;
+    }
+    setFavorites([...favorites, hero]);
+  };
+
+  const isFavorite = (hero: Hero) => {
+    return favorites.some((h) => h.id === hero.id);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  return (
+    <FavoriteHeroContext
+      value={{
+        favoriteCount: favorites.length,
+        favorites: favorites,
+        isFavorite: isFavorite,
+        toggleFavorite: toggleFavorites,
+      }}
+    >
+      {children}
+    </FavoriteHeroContext>
+  );
+};
